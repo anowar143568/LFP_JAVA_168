35499a4b892f78a93ecb970b52004b5ccb4cb6b4
Anowar <anowarulhasan58@gmail.com>
2022-06-30 22:40:51 +0530

    Code With Harry: Very good channel

---
+++ CodeWithHarry.java
@@ -0,0 +1,20 @@
+
+public class CodeWithHarry {
+
+	public static void main(String[] args) {
+		
+	byte age = 18;	
+    int age1 = 32;
+	float f1 = 5.6f;
+	char ch = 'A';
+	double d1 = 4.66D;
+	boolean a = true;
+	String str = "anowar"; 
+	System.out.print(a);
+	
+	System.out.println(str);
+	
+
+	}
+} 
+   
