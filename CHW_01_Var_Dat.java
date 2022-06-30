d2c97e80748b7a3255b4a32651391ee2ef698570
Anowar <anowarulhasan58@gmail.com>
2022-06-30 22:50:40 +0530

    Variable and Data Type : JAVA program

---
+++ CHW_01_Var_Dat.java
@@ -0,0 +1,16 @@
+
+public class CHW_01_Var_Dat {
+
+	public static void main(String[] args) {
+
+		System.out.print("The sum of the number is: ");
+
+		int num1 = 1;
+		int num2 = 2;
+		int num3 = 3;
+		int sum = num1 + num2 + num3;
+
+		System.out.println(sum);
+	}
+
+}
